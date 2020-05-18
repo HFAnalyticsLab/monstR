1aab5c088e98dc41bb40166166ed6956b2886159
Neale Swinnerton <neale@mastodonc.com>
2020-05-18 12:24:42 +0100

    Initial cut of packages required.

---
+++ src/0_install_packages.R
@@ -0,0 +1,12 @@
+## requires libssl-dev & libcurl4-openssl-dev on Linux
+## TODO - what is the equivalent on Windows?
+
+install.packages(c("devtools",
+                   "tidyverse",
+                   "here",
+                   "polite",
+                   "janitor"))
+
+## TODO - probably not needed in the pipeline code, but we have it for
+## now until refactoring complete.
+devtools::install_github('THF-evaluative-analytics/THFstyle')
