7270145e971fde4f62228c98b56fa22cba700930
Neale Swinnerton <neale@mastodonc.com>
2020-06-16 21:44:23 +0100

    Bug fixes.

--- R/util.R
+++ R/util.R
@@ -1,17 +1,17 @@
 
 ##' @title Safe Download
 ##'
-##' Downloads a file and tries hard to tidy up in
-##' the event of errors. Since these files are typically large we don't
-##' want to leave them in temp directories.
+##' Downloads a file and tries hard to tidy up in the event of
+##' errors. Since these files are typically large we don't want to
+##' leave them in temp directories.
 ##'
 ##' The destfile should only appear if the download was successful.
 ##'
 ##' @param url src for the download
 ##' @param destfile destination filename
-##' @param fvalidate a fn that is passed the filename after download to
-##'     validate it in some way. The fn should return TRUE if the file
-##'     is valid.
+##' @param fvalidate a fn that is passed the filename after download
+##'     to validate it in some way. The fn should return TRUE if the
+##'     file is valid.
 ##' @importFrom curl curl_download
 safe_download <- function(url, destfile, fvalidate) {
     success <- TRUE
@@ -23,12 +23,14 @@ safe_download <- function(url, destfile, fvalidate) {
 
         if (!missing(fvalidate) && !fvalidate(tmp)) {
             success <- FALSE
-            ## report the destfile name to not confuse user, although not strictly true
+            ## report the destfile name to not confuse user, although
+            ## not strictly true
             log_panic("file ", destfile, " failed validation. Deleting it")
         }
 
-        ## rename to final destination. This is generally an atomic operation, so
-        ## we can assume the final file only appears if this succeeds.
+        ## rename to final destination. This is generally an atomic
+        ## operation, so we can assume the final file only appears if
+        ## this succeeds.
         if (success && !file.rename(from = tmp,
                                     to = destfile)) {
             success <- FALSE
@@ -42,11 +44,13 @@ safe_download <- function(url, destfile, fvalidate) {
 
 #' @title Write Metadata
 #'
-#' \code{(write_metadata)} writes some metadata about where the file came from.
-#' TODO - could do this with fs xattr, but maybe that's not well known by users?
+#' \code{(write_metadata)} writes some metadata about where the file
+#' came from.  TODO - could do this with fs xattr, but maybe that's
+#' not well known by users?
 #'
 #' @param metadata a dataframe containing metadata
-#' @param destfile filename into which the metadata should be written as JSON
+#' @param destfile filename into which the metadata should be written
+#'     as JSON
 write_metadata <- function(metadata, destfile) {
     json <- jsonlite::toJSON(metadata, pretty = TRUE, flatten = TRUE)
     tryCatch({
@@ -60,14 +64,15 @@ write_metadata <- function(metadata, destfile) {
 
 ##' @title generate a filename for a download
 ##'
-##' @import whisker
 ##' @param template \link{whisker} template
 ##' @param root the root of the directory hierarchy
 ##' @param data data used to populate the template
 ##' @param create_directory boolean indicating whether to
 ##'     (recursively) create the directory hierarchy.
 ##' @return a filename
+##' @import whisker
 generate_download_filename <- function(template, root, data, create_directory=TRUE) {
+
     path <- whisker.render(template,
                            data)
 
@@ -88,14 +93,27 @@ generate_download_filename <- function(template, root, data, create_directory=TR
 ##'     (recursively) create the directory hierarchy.
 ##' @return boolean indicating success
 ##' @author Neale Swinnerton <neale@mastodonc.com
-write_csv <- function(thf,create_directory) {
+##' @import logger
+write_csv <- function(data, thf, create_directory) {
+    success <- TRUE
     thf$format <- "csv"
-    destfile <- generate_download_filename(thf$clean_data_filename_template,
-                                           thf$root,
+
+    destfile <- generate_download_filename(thf$clean_filename_template,
+                                           thf$download_root,
                                            thf,
                                            create_directory)
-    write.csv(thf$data, file=destfile)
+    logger::log_info(sprintf("Writing %s data to %s", thf$format,  destfile))
+
+    tryCatch (
+        write.csv(data, file=destfile),
+        error = function(e) {
+            success <- FALSE
+        }
+    )
+
+    success
 }
+
 
 ##' @title write the data as a xlsx.
 ##' @param thf dataframe created by the pipeline
@@ -104,13 +122,23 @@ write_csv <- function(thf,create_directory) {
 ##' @return boolean indicating success
 ##' @author Neale Swinnerton <neale@mastodonc.com>
 ##' @import writexl
-write_xlsx <- function(thf, create_directory) {
+##' @import logger
+write_xlsx <- function(data, thf, create_directory) {
+    success <- TRUE
     thf$format <- "xlsx"
-    destfile <- generate_download_filename(thf$clean_data_filename_template,
-                                           thf$root,
+    destfile <- generate_download_filename(thf$clean_filename_template,
+                                           thf$download_root,
                                            thf,
                                            create_directory)
-    writexl::write_xlsx(thf$data, path=destfile)
+    logger::log_info(sprintf("Writing %s data to %s", thf$format,  destfile))
+    tryCatch (
+        writexl::write_xlsx(x=data, path=destfile),
+        error = function(e) {
+            logger::log_error("Problem writing xlsx")
+            success <- FALSE
+        })
+
+    success
 }
 
 ##' @title write the data as a RDS.
@@ -119,11 +147,23 @@ write_xlsx <- function(thf, create_directory) {
 ##'     (recursively) create the directory hierarchy.
 ##' @return boolean indicating success
 ##' @author Neale Swinnerton <neale@mastodonc.com>
-write_rds <- function(thf,create_directory) {
+##' @import logger
+write_rds <- function(data, thf,create_directory) {
+    success <- TRUE
     thf$format <- "rds"
-    destfile <- generate_download_filename(thf$clean_data_filename_template,
-                                           thf$root,
+    destfile <- generate_download_filename(thf$clean_filename_template,
+                                           thf$download_root,
                                            thf,
                                            create_directory)
-    saveRDS(thf$data, file=destfile)
+    logger::log_info(sprintf("Writing %s data to %s", thf$format,  destfile))
+    tryCatch (
+        saveRDS(object=data, file=destfile),
+
+        error = function(e) {
+            logger::log_error("Problem writing rds")
+            success <- FALSE
+        }
+)
+
+    success
 }
